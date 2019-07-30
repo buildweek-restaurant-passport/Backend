97f5fc7e09441d0ddb595c81f6f660ffc2c4c305
abidex4yemi <abidex4yemi@gmail.com>
2019-07-30 13:21:58 +0100

    Complete add new restaurant endpoint

--- src/server/controllers/restaurants/addRestaurant.js
+++ src/server/controllers/restaurants/addRestaurant.js
@@ -1,5 +1,5 @@
 const { Restaurant } = require('../../models');
-const { createError, GENERIC_ERROR } = require('../../util/error');
+const { createError, GENERIC_ERROR, CONFLICT } = require('../../util/error');
 
 /**
  * Add new restaurant
@@ -8,11 +8,11 @@ const { createError, GENERIC_ERROR } = require('../../util/error');
  * @param {*} res
  * @param {*} next
  */
-const addRestaurant = (req, res, next) => {
+const addRestaurant = async (req, res, next) => {
   try {
     const restaurantDetails = req.body.sanitizedBody;
 
-    const restaurant = Restaurant.insert(restaurantDetails);
+    const restaurant = await Restaurant.insert(restaurantDetails);
 
     return res.status(201).json({
       success: true,
@@ -20,6 +20,15 @@ const addRestaurant = (req, res, next) => {
       body: restaurant,
     });
   } catch (error) {
+    if (error.code && error.code === 'SQLITE_CONSTRAINT') {
+      return next(
+        createError({
+          message: `Restaurant with name: ${req.body.name} already exist`,
+          status: CONFLICT,
+        }),
+      );
+    }
+
     return next(
       createError({
         message: 'Could not create new restaurant',
